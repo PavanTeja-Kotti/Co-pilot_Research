c3c4bd302d7c094dd47c4e278111314497360761
ankit435 <ankitkumarsingh435@gmail.com>
2025-01-24 15:55:40 +0530

    logoadded

---
+++ Client/copilotapp/src/asset/HeaderLogo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './loggogog.jpeg';
+
+const Logo = ({ className = '' }) => {
+ const logoStyle = {
+   width: '200px',
+   height: '50px',
+   objectFit: 'contain',
+   display: 'flex',
+   alignItems: 'left',
+   backgroundColor: 'inherit', // Dark background
+   padding: '0.5rem'
+ };
+
+ return (
+   <img  
+     src={logo} 
+     alt="Research Assistant Logo" 
+     style={logoStyle}
+     className={className} 
+   />
+ );
+};
+
+export default Logo;
